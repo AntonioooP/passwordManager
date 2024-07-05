d9b48d004dd6df99f5ca5415c7a36395dd94e8bd
antonii1357 <antonii.spam@gmail.com>
2024-07-04 21:22:22 -0600

    Moved functions and left listeners. generatedPassword listener for a the copied message div was moved to copyToClipboard global function

--- extension/popup.js
+++ extension/popup.js
@@ -56,59 +56,10 @@ document.addEventListener('DOMContentLoaded', function () {
 					})
 				)
 
-				document.getElementById('retrievedData').innerText = JSON.stringify(decryptedData, null, 2)
+				populateTable(decryptedData)
 			})
 	})
 })()
-
-function encrypt(text, key) {
-	const iv = crypto.getRandomValues(new Uint8Array(16))
-	const cipher = new TextEncoder().encode(text)
-	return window.crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['encrypt']).then((cryptoKey) =>
-		window.crypto.subtle
-			.encrypt(
-				{
-					name: 'AES-GCM',
-					iv: iv
-				},
-				cryptoKey,
-				cipher
-			)
-			.then((encrypted) => {
-				const encryptedArray = new Uint8Array(encrypted)
-				const result = new Uint8Array(iv.byteLength + encryptedArray.byteLength)
-				result.set(iv, 0)
-				result.set(encryptedArray, iv.byteLength)
-				return btoa(String.fromCharCode.apply(null, result))
-			})
-	)
-}
-
-function decrypt(text, key) {
-	const data = atob(text)
-		.split('')
-		.map((char) => char.charCodeAt(0))
-	const iv = new Uint8Array(data.slice(0, 16))
-	const encryptedData = new Uint8Array(data.slice(16))
-	return window.crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['decrypt']).then((cryptoKey) =>  window.crypto.subtle
-			.decrypt(
-				{
-					name: 'AES-GCM',
-					iv: iv
-				},
-				cryptoKey,
-				encryptedData
-			)
-			.then((decrypted) => new TextDecoder().decode(decrypted))
-	)
-}
-function generatePassword(length = 16) {
-	const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-='
-	const password = []
-	const values = crypto.getRandomValues(new Uint8Array(length))
-	for (let i = 0; i < length; i++) password.push(charset[values[i] % charset.length])
-	return password.join('')
-}
 
 document.getElementById('generate').addEventListener('click', async function () { 
 	const password = generatePassword()
@@ -116,32 +67,10 @@ document.getElementById('generate').addEventListener('click', async function () 
 	passwordField.innerText = password
 	passwordField.classList.remove('hidden')
 })
-document.getElementById('generatedPassword').addEventListener('click', async () => {
-	// Create the "Copied!" message div
-	const copiedMessage = document.createElement('div')
-	copiedMessage.innerText = 'Copied!'
-	copiedMessage.style.position = 'absolute'
-	copiedMessage.style.background = 'rgba(0, 0, 0, 0.8)'
-	copiedMessage.style.color = 'white'
-	copiedMessage.style.padding = '5px 10px'
-	copiedMessage.style.borderRadius = '4px'
-	copiedMessage.style.zIndex = '1000'
 
-	const rect = document.getElementById('generatedPassword').getBoundingClientRect()
-	copiedMessage.style.top = `${rect.top - 30}px` 
-	try {
+document.getElementById('generatedPassword').addEventListener('click', async (event) => {
+	const element = event.target
+	const text = element.innerText
 
-		await navigator.clipboard.writeText(document.getElementById('generatedPassword').innerText)
-	} catch (er) {
-		console.log('Failed to copy password.', er)
-	}
-		
-	document.body.appendChild(copiedMessage)
-
-	setTimeout(function () {
-		copiedMessage.remove()
-	}, 2000)
+	await copyToClipboard(text, element)
 })
-
-
-const deterministicEncrypt = async (text, key) => Array.from(new Uint8Array(await crypto.subtle.sign("HMAC", await crypto.subtle.importKey("raw", new TextEncoder().encode(key), { name: "HMAC", hash: "SHA-256" }, false, ["sign"]), new TextEncoder().encode(text)))).map(b => b.toString(16).padStart(2, '0')).join('')
